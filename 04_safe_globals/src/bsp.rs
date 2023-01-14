7c22f608e585a8d9ed1a086ca1e59b5ea64f32ff
YuLong Yao <feilongphone@gmail.com>
2023-01-15 07:02:40 +0800

    04_safe_globals

--- 04_safe_globals/src/bsp.rs
+++ 04_safe_globals/src/bsp.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 //
 // Copyright (c) 2018-2022 Andre Richter <andre.o.richter@gmail.com>
+// Copyright (c) 2023 YuLong Yao <feilongphone@gmail.com>
 
 //! Conditional reexporting of Board Support Packages.
 
@@ -9,3 +10,9 @@ mod raspberrypi;
 
 #[cfg(any(feature = "bsp_rpi3", feature = "bsp_rpi4"))]
 pub use raspberrypi::*;
+
+#[cfg(any(feature = "bsp_mangopi_mq_pro"))]
+mod mangopi_mq_pro;
+
+#[cfg(any(feature = "bsp_mangopi_mq_pro"))]
+pub use mangopi_mq_pro::*;
---
+++ 04_safe_globals/src/bsp/mangopi_mq_pro.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2022-2023 YuLong Yao <feilongphone@gmail.com>
+
+//! Top-level BSP file for the MangoPi Mq Pro.
+
+pub mod console;
+pub mod cpu;
