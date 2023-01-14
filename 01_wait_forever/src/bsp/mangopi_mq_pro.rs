ece62048fb8fe44c9525bd669e2724a9d9ce7cb7
YuLong Yao <feilongphone@gmail.com>
2023-01-15 06:05:11 +0800

    01_wait_forever: add mangopi_mq_pro board support

---
+++ 01_wait_forever/src/bsp/mangopi_mq_pro.rs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2022-2023 YuLong Yao <feilongphone@gmail.com>
+
+//! Top-level BSP file for the MangoPi Mq Pro.
+
+// Coming soon.
--- 01_wait_forever/src/cpu/boot.rs
+++ 01_wait_forever/src/cpu/boot.rs
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 //
 // Copyright (c) 2021-2022 Andre Richter <andre.o.richter@gmail.com>
+// Copyright (c) 2022 YuLong Yao <feilongphone@gmail.com>
 
 //! Boot code.
 
 #[cfg(target_arch = "aarch64")]
 #[path = "../_arch/aarch64/cpu/boot.rs"]
+mod arch_boot;
+
+#[cfg(target_arch = "riscv64")]
+#[path = "../_arch/riscv64/cpu/boot.rs"]
 mod arch_boot;
